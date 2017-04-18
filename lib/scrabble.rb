1a6e6593a295657ff43a158d97d1fd1f370eea69
Aubrey Masten and Jennifer Kinsey <jennifer@>
2017-04-18 09:36:24 -0700

    add code and spec to calculate score of any single letter

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,10 +1,24 @@
 class String
   define_method(:scrabble) do
-    score = 0
-    score_table = {1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] }
-    if score_table.fetch(1).include?(self.upcase)
-      score = score.+(1)
+    total_score = 0
+    score_table = {
+      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+      2 => ["D", "G"],
+      3 => ["B", "C", "M", "P"],
+      4 => ["F", "H", "V", "W", "Y"],
+      5 => ["K"],
+      8 => ["J", "X"],
+      10 => ["Q", "Z"]}
+
+    # if score_table.fetch(1).include?(self.upcase)
+    #   total_score = score.+(1)
+    # end
+
+    score_table.each_pair() do |score, letters|
+      if letters.include?(self.upcase)
+        total_score = total_score.+(score)
+      end
     end
-    score
+    total_score
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,6 +5,9 @@ describe("String#scrabble") do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble()).to(eq(1))
   end
+  it("returns a scrabble score for any given letter") do
+    expect("j".scrabble()).to(eq(8))
+  end
 
   # it("returns a scrabble score for a word") do
   #   expect("cat".scrabble()).to(eq(5))
